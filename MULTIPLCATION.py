0e283341c42a6a62b151f7a8fe2120cd55c3116e
Paul Aron Tamayo <tamayo.paularonb.bsis@gmail.com>
2026-03-01 23:38:10 +0800

    Add files via upload

---
+++ MULTIPLCATION.py
@@ -0,0 +1,8 @@
+print("=================================")
+number_input1 = int(input("Enter first number: "))
+number_input2 = int(input("Enter second number: "))
+print("=================================")
+
+input_product = number_input1 * number_input2
+
+print(f'The product of numbers is: {input_product}')
