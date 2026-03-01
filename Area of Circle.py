88404638d421bc47499fa13d5d117b659912e9c2
Paul Aron Tamayo <tamayo.paularonb.bsis@gmail.com>
2026-03-01 23:35:34 +0800

    Add files via upload

---
+++ Area of Circle.py
@@ -0,0 +1,7 @@
+import math
+pi = math.pi
+print("=================================")
+radius_input = int(input("Enter radius: "))
+print("=================================")
+area_answer = pi * radius_input ** 2
+print(f'The area of the circle is: {area_answer:.2f}')
